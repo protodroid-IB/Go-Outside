ff6bf85cb4046d1a872f8603528a6a85b401c3a8
Laurence <101215@student.sae.edu.au>
2019-02-26 14:26:04 +0800

    started on greybox, road pieces done, imported into unity and configured, sphere mask shaders applied

---
+++ GoOutside_Unity2018.3.2f1/Assets/CameraMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraMovement : MonoBehaviour
+{
+    private PlayerMovementController playerMovementController;
+    private Vector3 playerPosition = Vector3.zero;
+
+    [SerializeField]
+    private float heightAbovePlayer = 20f;
+
+    [SerializeField]
+    private float distanceFromPlayer = 30f;
+
+
+    private void Start()
+    {
+        playerMovementController = GameObject.FindWithTag("Player").GetComponent<PlayerMovementController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerMovementController != null)
+            SetPlayerPosition(playerMovementController.transform.position);
+        else
+            SetPlayerPosition(GameObject.FindWithTag("Player").GetComponent<PlayerMovementController>().transform.position);
+        MoveCamera();   
+    }
+
+    private void MoveCamera()
+    {
+        transform.position = new Vector3(playerPosition.x, playerPosition.y + heightAbovePlayer, playerPosition.z - distanceFromPlayer);
+    }
+
+    public void SetPlayerPosition(Vector3 inPosition)
+    {
+        playerPosition = inPosition;
+    }
+}
---
+++ GoOutside_Unity2018.3.2f1/Assets/Shaders/SphereMask.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SphereMask : MonoBehaviour
+{
+    public float maxRadius = 2f;
+    public float speed = 10f;
+
+    public bool ossiclateEffect = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float radius;
+
+        if (ossiclateEffect)
+            radius = (maxRadius / 2) * Mathf.Sin(Time.time * speed / 100f) + (maxRadius / 2f);
+        else
+            radius = maxRadius;
+
+
+        Shader.SetGlobalFloat("GLOBALMASK_Radius", radius);
+        Shader.SetGlobalVector("GLOBALMASK_Position", new Vector3(transform.position.x, transform.position.y - 0.3f, transform.position.z));
+    }
+}
