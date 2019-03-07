9875df6c55cf2bd284dc89678a7297e4a20a85b9
Laurence <101215@student.sae.edu.au>
2019-03-07 16:17:07 +0800

    implemented the school drop off zone for lil sister

---
+++ GoOutside_Unity2018.3.2f1/Assets/SisterCollisions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SisterCollisions : MonoBehaviour
+{
+    private SisterManager sisterManager;
+
+    private void Start()
+    {
+        sisterManager = transform.parent.GetComponent<SisterManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("SchoolSisterZone"))
+        {
+            if(!sisterManager.GetDroppedAtSchool())
+            {
+                sisterManager.SetDroppedAtSchool(true);
+            }
+        }
+    }
+}
---
+++ GoOutside_Unity2018.3.2f1/Assets/SisterManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SisterManager : MonoBehaviour
+{
+    private bool droppedAtSchool = false;
+    private bool backHome = false;
+
+
+
+
+    public void SetDroppedAtSchool(bool inBool)
+    {
+        droppedAtSchool = inBool;
+        GlobalReferences.instance.errandManager.SisterArrivedAtSchool();
+    }
+
+    public void SetBackHome(bool inBool)
+    {
+        backHome = inBool;
+        GlobalReferences.instance.errandManager.SisterBackFromSchool();
+    }
+
+
+
+
+    public bool GetDroppedAtSchool()
+    {
+        return droppedAtSchool;
+    }
+
+    public bool GetBackHome()
+    {
+        return backHome;
+    }
+
+
+    
+}
