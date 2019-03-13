e84efd2be57e4f48724b0a6408246493d34dcd95
Laurence <40289918+protodroid-IB@users.noreply.github.com>
2019-03-13 11:28:02 +0800

    started on dialogue system

--- GoOutside_Unity2018.3.2f1/Assets/Scripts/DialogueManager.cs
+++ GoOutside_Unity2018.3.2f1/Assets/Scripts/DialogueManager.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class DialogueManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject dialogueGO;
+
+    private Animator dialogueAnimator;
+
     private bool dialogueActive = false;
 
+    private void Start()
+    {
+        dialogueAnimator = dialogueGO.GetComponent<Animator>();
 
+        UpdateDialogue();
+    }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             dialogueActive = !dialogueActive;
+
+            
+        }
+    }
+
+
+    private void UpdateDialogue()
+    {
+        if (dialogueActive)
+        {
+            StartDialogue("Hello, I am the biggest of dads. Welcome to dadville!");
+        }
+        else
+        {
+            EndDialogue();
         }
     }
 
@@ -20,5 +45,26 @@ public class DialogueManager : MonoBehaviour
     public bool IsDialogueActive()
     {
         return dialogueActive;
+    }
+
+
+    public void StartDialogue(string inDialogue)
+    {
+        WindowActivate(true);
+    }
+
+    public void EndDialogue()
+    {
+        WindowActivate(false);
+    }
+
+
+
+    private void WindowActivate(bool inActive)
+    {
+        if (inActive)
+            dialogueAnimator.SetTrigger("Idle");
+        else
+            dialogueAnimator.SetTrigger("Off");
     }
 }
